ef9f1056a7802d6ea84b1f32bc9c1f097e9c3922
Sudeep Gurung <sudeepgurung.sg@gmail.com>
2025-07-14 15:37:38 +0545

    wip

---
+++ app/components/content/HomeFeature.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="flex items-start gap-3">
+        <div class="flex items-center justify-center border rounded-lg p-1.5">
+            <UIcon :name="icon" />
+        </div>
+        <div class="flex flex-col">
+            <h3 class="font-semibold">
+                <ContentSlot name="title" />
+            </h3>
+            <span>
+                <ContentSlot name="description" />
+            </span>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+    icon: {
+        type: String,
+        default: 'i-ph-cursor-click',
+    },
+})
+</script>
