12c9eec90bd5492f1d3b0ec5be6f9e035e234bd4
Sudeep Gurung <sudeepgurung.sg@gmail.com>
2025-07-14 23:07:43 +0545

    wip

--- app/components/content/HomeFeature.vue
+++ app/components/content/HomeFeature.vue
@@ -1,3 +1,19 @@
+<template>
+    <div class="flex items-start gap-3">
+        <div class="flex items-center justify-center border rounded-lg p-1.5">
+            <UIcon :name="icon" />
+        </div>
+        <div class="flex flex-col">
+            <h3 class="font-semibold">
+                <ContentSlot :use="$slots.title" unwrap />
+            </h3>
+            <span>
+                <ContentSlot :use="$slots.description" unwrap />
+            </span>
+        </div>
+    </div>
+</template>
+
 <script setup lang="ts">
 defineProps({
     icon: {
@@ -6,21 +22,3 @@ defineProps({
     },
 })
 </script>
-
-<template>
-    <div class="flex items-start gap-3 border p-2 rounded bg-gray-50">
-        <div class="flex items-center justify-center border rounded-lg p-1.5">
-            <UIcon :name="icon" />
-        </div>
-        <div class="flex flex-col">
-            <h3 class="font-semibold">
-                <ContentSlot name="title" />
-            </h3>
-            <span>
-                <ContentSlot name="description" />
-            </span>
-        </div>
-        <!-- DEBUGGING SLOT DATA -->
-        <pre>{{ Object.keys($slots) }}</pre>
-    </div>
-</template>
