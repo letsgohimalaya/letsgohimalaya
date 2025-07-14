28d8481adeed9ebe3c1c9d69f2ab2e6063f2560e
letsgohimalaya <letsgohimalaya.com.np@gmail.com>
2025-07-14 13:05:17 +0545

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: {
+    preset: 'github_pages' // or 'static'
+  },
   modules: [
     '@nuxt/content',
   ],
